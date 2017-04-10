6b5cc38c44c8f262edbbf7f9012d830fb0c7a2bf
wutianfeng2017 <kww12@sina.com>
2017-04-10 22:02:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wtf2016/myapplicationsdf/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wtf2016.myapplicationsdf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
